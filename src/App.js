9615904976f1aa3fde4dac0e01eb0c3481cb387b
jswan <jswannieman@gmail.com>
2023-07-24 17:22:07 +1200

    Update App Javascript Component

--- src/App.js
+++ src/App.js
@@ -3,26 +3,22 @@ import React, { useState, useRef, useLayoutEffect } from "react";
 function App() {
   const [show, setShow] = useState(false);
   const h3Ref = useRef();
+  const buttonRef = useRef();
 
   useLayoutEffect(() => {
-    if (show) {
-      h3Ref.current.style.margin = "1rem 30rem";
+    if (h3Ref.current == null || buttonRef.current == null) {
+      return;
     }
+    const { bottom } = buttonRef.current.getBoundingClientRect();
+    h3Ref.current.style.top = `${bottom + 150}px`;
   }, [show]);
 
-  const h3Style1 = {
-    margin: "1rem 2rem",
-    color: "Red",
-  };
-
-  const h3Style2 = {
+  const h3Style = {
+    position: "absolute",
     color: "Red",
   };
 
   const buttonStyle = {
-    position: "absolute",
-    top: "10rem",
-    left: "2rem",
     color: "#FFF",
     backgroundColor: "navy",
     padding: ".8rem",
@@ -30,16 +26,18 @@ function App() {
 
   return (
     <div className="App">
-      {!show ? (
-        <h3 style={h3Style1}>We can now show the code at this position</h3>
-      ) : (
-        <h3 style={h3Style2} ref={h3Ref}>
+      <button
+        ref={buttonRef}
+        style={buttonStyle}
+        onClick={() => setShow(!show)}
+      >
+        Toggle
+      </button>
+      {show && (
+        <h3 ref={h3Ref} style={h3Style}>
           We can now show the code at this position
         </h3>
       )}
-      <button onClick={() => setShow(!show)} style={buttonStyle}>
-        Toggle
-      </button>
     </div>
   );
 }
