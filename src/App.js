f355ea471cccb7844184bb2c6431c6dab51fbc8c
jswan <jswannieman@gmail.com>
2023-07-24 16:55:33 +1200

    Commit App Javascript Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, useRef, useLayoutEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [show, setShow] = useState(false);
+  const h3Ref = useRef();
+
+  useLayoutEffect(() => {
+    if (show) {
+      h3Ref.current.style.margin = "1rem 30rem";
+    }
+  }, [show]);
+
+  const h3Style1 = {
+    margin: "1rem 2rem",
+    color: "Red",
+  };
+
+  const h3Style2 = {
+    color: "Red",
+  };
+
+  const buttonStyle = {
+    position: "absolute",
+    top: "10rem",
+    left: "2rem",
+    color: "#FFF",
+    backgroundColor: "navy",
+    padding: ".8rem",
+  };
+
+  return (
+    <div className="App">
+      {!show ? (
+        <h3 style={h3Style1}>We can now show the code at this position</h3>
+      ) : (
+        <h3 style={h3Style2} ref={h3Ref}>
+          We can now show the code at this position
+        </h3>
+      )}
+      <button onClick={() => setShow(!show)} style={buttonStyle}>
+        Toggle
+      </button>
+    </div>
+  );
 }
 
 export default App;
